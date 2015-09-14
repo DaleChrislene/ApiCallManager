cb5ac49f6e28325ae6768fd690e6d9956987dc01
dalec <summer.dchrislene@gmail.com>
2015-09-14 13:30:25 +0800

    indentation

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -1,17 +1,16 @@
-require 'net/http'
+require "net/http"
 
 module Api
- 	module V1
-    	class AirlinesController < ApplicationController
-    		include AirlinesHelper
-
+    module V1
+        class AirlinesController < ApplicationController
+            include AirlinesHelper
             skip_before_filter :verify_authenticity_token
                         
-    		def search
-    			#return response to FrontEnd
+            def search
+                #return response to FrontEnd
                 render json: call_post_api(params)   
-    		end
-    	
+            end
+        
         end
     end
 end
